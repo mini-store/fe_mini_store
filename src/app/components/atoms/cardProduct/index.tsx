d64ba4e9c9d2ee1d4a75365f313f8146a462af3f
abayu_dev <adinatabayu760@gmail.com>
2023-02-02 01:35:28 +0700

    [FEATURE] Create card category product

---
+++ src/app/components/atoms/cardProduct/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function CardProduct(param: object) {
+  return (
+    <button className="w-24 h-24 rounded-lg bg-neutral-100 justify-center items-center flex flex-col place-content-between hover:bg-slate-300">
+          <Image src={param.linkIcon} alt={param.iconName} />
+          <h1>{param.iconName}</h1>
+    </button>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import './globals.css';
+import './styles/globals.css';
+import CardProduct  from "../app/components/atoms/cardProduct";
+import linkIcon from '../../assets/icons/tas.png';
 
 export default function Page() {
+  const data = {
+    "linkIcon": linkIcon,
+    "iconName": "Tas"
+  }
   return (
-    <h1 className="text-blue-600 text-3xl font-bold">
-      Test
-    </h1>
+    <div>
+      <CardProduct {...data} />
+    </div>
   )
 }
