723a90fe8ef28392cecb02f285209793833d8074
evribsap <evribsaputra@gmail.com>
2023-02-02 21:58:45 +0700

    testing git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by Evri&nbsp;
+          Get started by Evri Bagas Saputro&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
