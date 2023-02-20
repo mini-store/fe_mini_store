b221ec727560b956ddf9a981c2097a1928c76e31
devbayu20 <v130421015@bussan.co.id>
2023-02-20 16:53:33 +0700

    [FEATURE] Add UI List Products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import "./styles/globals.css";
 import CardProduct from "../app/components/atoms/cardProduct";
 import linkIcon from "../../assets/icons/tas.png";
 import CategoryProduct from "../app/components/molecules/categoryProduct";
+import Link from "next/link";
 
 export default function Page() {
   const data = [
@@ -34,6 +35,7 @@ export default function Page() {
       </h1>
       {/* @ts-expect-error Server Component */}
       <CategoryProduct categories={data} />
+      <Link href="/listProducts">See More</Link>
     </div>
   )
 }
